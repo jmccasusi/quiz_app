d87608b3fac7d20f80100c7fc9170bc90c790ee1
jmccasusi <jmccasusi@gmail.com>
2019-08-08 11:47:55 -0400

    search questions by content working

--- controllers/teachers.js
+++ controllers/teachers.js
@@ -28,12 +28,12 @@ teachers.get('/question/:id', (req, res) => {
 		examModel.Exam.find({
 			owner_id: req.session.currentUser._id
 		}, (err, foundExams) => {
-				res.render('index.ejs', {
-					currentUser: req.session.currentUser,
-					pageToRender: "show_question",
-					ownedExams: foundExams,
-					currentQuestion: foundQuestion
-				})
+			res.render('index.ejs', {
+				currentUser: req.session.currentUser,
+				pageToRender: "show_question",
+				ownedExams: foundExams,
+				currentQuestion: foundQuestion
+			})
 		})
 	})
 });
@@ -41,11 +41,15 @@ teachers.get('/question/:id', (req, res) => {
 teachers.get('/question', (req, res) => {
 	questionModel.Question.find({
 		owner_id: req.session.currentUser._id
-	}, (err, foundQuestions) => {
-		console.log(foundQuestions);
+	}, (err, ownedQuestions) => {
+		let ownedQuestionLimit = 5;
+			if(ownedQuestionLimit>ownedQuestions.length){
+				ownedQuestionLimit = ownedQuestions.length;
+			}
+			ownedQuestions = ownedQuestions.slice(ownedQuestions.length-ownedQuestionLimit)
 		res.render('index.ejs', {
 			currentUser: req.session.currentUser,
-			ownedQuestions: foundQuestions,
+			ownedQuestions: ownedQuestions,
 			pageToRender: "question"
 		});
 	})
@@ -64,12 +68,55 @@ teachers.post('/question/new', (req, res) => {
 	});
 });
 
+teachers.post('/question', (req, res) => {
+	userModel.User.find({}, (err, foundUsers) => {
+		questionModel.Question.find({}, (err, foundQuestions) => {
+			questionModel.Question.find({
+				owner_id: req.session.currentUser._id
+			}, (err, ownedQuestions) => {
+				let ownedQuestionLimit = 5;
+				if(ownedQuestionLimit>ownedQuestions.length){
+					ownedQuestionLimit = ownedQuestions.length;
+				}
+				ownedQuestions = ownedQuestions.slice(ownedQuestions.length-ownedQuestionLimit)
+				const searchResultQuestions = [];
+				foundQuestions.forEach((foundQuestion) => {
+					if (foundQuestion.content.toLowerCase().indexOf(req.body.searchKeyword.toLowerCase()) >= 0) {
+						searchResultQuestions.push(foundQuestion);
+					}
+				})
+
+				searchResultQuestions.forEach((searchResult) => {
+					foundUsers.forEach((foundUser) => {
+						if(foundUser._id==searchResult.owner_id){
+							searchResult.owner_name = `${foundUser.firstName} ${foundUser.lastName}`;
+						}
+					})
+				})
+
+				res.render('index.ejs', {
+					currentUser: req.session.currentUser,
+					ownedQuestions: ownedQuestions,
+					searchResults: searchResultQuestions.reverse(),
+					pageToRender: "question"
+				});
+			})
+		})
+	})
+});
+
 teachers.put('/exam/addQuestion/:id', (req, res) => {
-	examModel.Exam.findByIdAndUpdate(req.body.targetExamId, {$addToSet:{questions_ids: req.params.id}}, { new: true }, (err, foundExam) => {
-		if(err){
+	examModel.Exam.findByIdAndUpdate(req.body.targetExamId, {
+		$addToSet: {
+			questions_ids: req.params.id
+		}
+	}, {
+		new: true
+	}, (err, foundExam) => {
+		if (err) {
 			console.log(err);
 		} else if (foundExam) {
-			res.redirect(`/teachers/question`); 
+			res.redirect(`/teachers/question`);
 		}
 	});
 });
@@ -140,21 +187,33 @@ teachers.post('/exam/new', (req, res) => {
 });
 
 teachers.put('/group/addExam/:id', (req, res) => {
-	groupModel.Group.findByIdAndUpdate(req.body.targetGroupId, {$addToSet:{exams_ids: req.params.id}}, { new: true }, (err, foundGroup) => {
-		if(err){
+	groupModel.Group.findByIdAndUpdate(req.body.targetGroupId, {
+		$addToSet: {
+			exams_ids: req.params.id
+		}
+	}, {
+		new: true
+	}, (err, foundGroup) => {
+		if (err) {
 			console.log(err);
 		} else if (foundGroup) {
-			res.redirect(`/teachers/exam/${req.params.id}`); 
+			res.redirect(`/teachers/exam/${req.params.id}`);
 		}
 	});
 });
 
 teachers.put('/group/:groupId/removeExam/:examId', (req, res) => {
-	groupModel.Group.findByIdAndUpdate(req.params.groupId, {"$pull": { "exams_ids": req.params.examId }}, { new: true }, (err, updatedGroup) => {
-		if(err){
+	groupModel.Group.findByIdAndUpdate(req.params.groupId, {
+		"$pull": {
+			"exams_ids": req.params.examId
+		}
+	}, {
+		new: true
+	}, (err, updatedGroup) => {
+		if (err) {
 			console.log(err);
 		} else {
-			res.redirect(`/teachers/group/${req.params.groupId}`);     
+			res.redirect(`/teachers/group/${req.params.groupId}`);
 		}
 	})
 });
@@ -184,18 +243,19 @@ teachers.get('/group/:id', (req, res) => {
 				$in: foundGroup.members_ids
 			}
 		}, (err, foundMembers) => {
-			examModel.Exam.find({_id: {
-				$in: foundGroup.exams_ids
-			}
-		}, (err, foundExams) => {
-			res.render('index.ejs', {
-				currentUser: req.session.currentUser,
-				pageToRender: "show_group",
-				currentGroup: foundGroup,
-				groupExams: foundExams,
-				groupMembers: foundMembers
+			examModel.Exam.find({
+				_id: {
+					$in: foundGroup.exams_ids
+				}
+			}, (err, foundExams) => {
+				res.render('index.ejs', {
+					currentUser: req.session.currentUser,
+					pageToRender: "show_group",
+					currentGroup: foundGroup,
+					groupExams: foundExams,
+					groupMembers: foundMembers
+				})
 			})
-		})
 		})
 	})
 });
